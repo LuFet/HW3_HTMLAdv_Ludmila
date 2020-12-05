4cb6af295f2e1cba37007c70c4c7f3dcf0214b51
LuFet <ludmila@bernet.name>
2020-12-05 22:19:51 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@
 	var el = document.getElementById("test");
 
 	var imgs = [];
-	var arr = ["images/1.png","images/2.png", "images/3.png", "images/4.png", "images/5.png"];
+	var arr = ["1.png","2.png", "3.png", "4.png", "5.png"];
 	
 		for (var i = 0; i < arr.length; i++) {
 
